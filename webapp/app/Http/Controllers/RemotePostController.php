506eda1604a158a9aa896cee8e7f673b3cb936d3
Shouta <zuomxiangtai668@gmail.com>
2024-07-25 22:19:34 +0900

    Lesson re do

--- webapp/app/Http/Controllers/RemotePostController.php
+++ webapp/app/Http/Controllers/RemotePostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use Illuminate\Support\Facades\DB;
 
 class RemotePostController extends Controller
 {
@@ -40,7 +41,7 @@ class RemotePostController extends Controller
 
             // ロールバック
             DB::rollback();
-            
+
             // エラーメッセージ
             return response()->json(['error' => $e->getMessage()], 500);
         }//
